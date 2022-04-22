90b1b5305b2197e1f51ed6a33f9963f92cecf4aa
Munir Abdinur <munir.abdinur@datadoghq.com>
2022-04-22 11:41:20 -0400

    test(telemetry): avoid importing pkg resources

--- ddtrace/internal/telemetry/writer.py
+++ ddtrace/internal/telemetry/writer.py
@@ -171,10 +171,10 @@ class TelemetryWriter(PeriodicService):
         """Sent when TelemetryWriter is enabled or forks"""
         # pkg_resources import is inlined for performance reasons
         # This import is an expensive operation
-        import pkg_resources
 
+        # import pkg_resources
         payload = {
-            "dependencies": [{"name": pkg.project_name, "version": pkg.version} for pkg in pkg_resources.working_set],
+            # "dependencies": [{"name": pkg.project_name, "version": pkg.version} for pkg in pkg_resources.working_set],
             "integrations": self._flush_integrations_queue(),
             "configurations": [],
         }
